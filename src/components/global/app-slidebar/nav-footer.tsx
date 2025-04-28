8ec6a4646bccb07702b39cc814f9bc7741db63c9
mahi0331 <vvce22cse0159@vvce.ac.in>
2025-04-28 22:22:56 +0530

    updated

--- src/components/global/app-slidebar/nav-footer.tsx
+++ src/components/global/app-slidebar/nav-footer.tsx
@@ -21,7 +21,16 @@ const NavFooter = ({prismaUser}: { prismaUser: User }) => {
   return <SidebarMenu>
     <SidebarMenuItem>
         <div className = "flex flex-col gap-y-6 items-start group-data-[collapsible=icon]:hidden">
-            {!prismaUser.subcription}
+            {!prismaUser.subcription && (
+                <div className="flex flex-col items-start p-2 pb-3 gap-4 bg-background-80"> 
+                        <div className = "flex flex-col items-start gap-1">
+                        <p className = "text-base font-bold">
+                            Get <span className = "text-vivid">Creative AI</span>
+                        </p>
+                        </div>
+                </div>
+            )}
+        </div>
     </SidebarMenuItem>
   </SidebarMenu>
 
