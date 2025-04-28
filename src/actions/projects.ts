68a681b13233aad0db4c99b0108fdcab1419713c
Vaishnavi3041 <vaishnavikm21@gmail.com>
2025-04-28 16:11:23 +0530

    Create projects.ts
    initial commit

---
+++ src/actions/projects.ts
@@ -0,0 +1,32 @@
+"use server"
+
+import {onAuthenticateUser} from './user'
+export const getAllProjects = async () => {
+    try{
+        const checkUser = await onAuthenticateUser()
+        if(checkUser.status!==200 || !checkUser.user){
+            return{status:403,error:'User Not Authenticated'}
+        }
+
+
+        const projects = await client.project.findMany({
+            where:{
+                userId:checkUser.user.id,
+                isDeleted : false,
+
+            },
+            orderBy:{
+                updatedAt:'desc',
+            },
+        })
+
+        if(projects.length === 0){
+            return{status:404,error:'No Projects Found'}
+        }
+
+        return{ status:200,data:projects}
+    }catch (error){
+        console.log('🔴 ERROR', error);
+        return { status: 500 , error:'Intenal Server Error'};
+    }
+}
