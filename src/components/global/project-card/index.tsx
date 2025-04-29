48608dc1ddf3458be683afb1fac85d222086c72e
Vaishnavi3041 <vaishnavikm21@gmail.com>
2025-04-29 15:38:58 +0530

    Updated

--- src/components/global/project-card/index.tsx
+++ src/components/global/project-card/index.tsx
@@ -15,17 +15,15 @@ type props = {
     projectId: string
     title: string 
     createdAt: string
-    src: string
+    themeName: string
     isDelete?: boolean
     slideData: JsonValue
-    themeName: string
 }
 
 const projectCard = ({
     createdAt,
     projectId,
     slideData,
-    src,
     title,
     themeName,
     isDelete
@@ -48,17 +46,63 @@ const projectCard = ({
         if(!projectId)
         {
             setLoading(false)
-            toast('Error', {
+            toast.error('Error', {
                 description: 'Project not found.',
             })
             return
         }
         try{
-            const res = await recoverProject(projectId);
+            const res = await recoverProject(projectId)
+            if(res.status!==200){
+                toast.error('Oppse!', {
+                    description: res.error || 'Something went wrong',
+                })
+                return 
+            }
+            setOpen(false)
+            router.refresh()
+            toast.success('Success', {
+                description: 'Project recovered successfully.',
+            })
         }catch(error){
+            console.log(error)
+            toast.error('Oppse!', {
+                description: 'Something went wrong.Please contact support. ',
+            })
 
         }
     } 
+
+    const handleDelete = () async () => {
+        setLoading(true)
+        if(!projectId){
+            setLoading(false)
+            toast.error('Error', {
+                description: 'Project not found.',
+            })
+            return
+        }
+        try{
+            const res = await deleteProject(projectId)
+            if(res.status!==200){
+                toast.error('Oppse!', {
+                    description: res.error || 'Failed to delete the project',
+                })
+                return 
+            }
+            setOpen(false)
+            router.refresh()
+            toast.success('Success', {
+                description: 'Project deleted successfully.',
+            })
+        }catch(error){
+            console.log(error)
+            toast.error('Oppse!', {
+                description: 'Something went wrong.Please contact support. ',
+            })
+
+        }
+    }
 
     return ( 
     <motion.div
@@ -88,7 +132,7 @@ const projectCard = ({
                     >
                         { timeAgo(createdAt)}
                     </p>
-                    {/* {isDelete ? ( */}
+                    {isDelete ? ( 
                         <AlertDialogBox description="This will recover your project and restore your data."
                         className="bg-green-500 text-white dark:bg-green-600 hover:bg-green-600 dark:hover:bg-green-700"
                         loading={loading}
@@ -105,9 +149,24 @@ const projectCard = ({
                                 Recover
                             </Button>
                         </AlertDialogBox>
-                    {/* ) : ( 
-                        '' 
-                    )} */}
+                     ) : ( 
+                        <AlertDialogBox description="This will recover your project and restore your data."
+                        className="bg-green-500 text-white dark:bg-green-600 hover:bg-green-600 dark:hover:bg-green-700"
+                        loading={loading}
+                        open={open}
+                        onClick={handleRecover}
+                        handleOpen={() => setOpen(!open)}
+                        >
+                            <Button
+                                size="sm"
+                                variant="ghost"
+                                className="bg-background-80 dark:hover:bg-background-90"
+                                disabled={loading}
+                            >
+                                Delete
+                            </Button>
+                        </AlertDialogBox> 
+                    )} 
                 </div>
             </div>
         </div>
