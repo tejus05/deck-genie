aebb3decf03c42c2e5f35f9358acb1061ef06640
AmithursR <amithurs88@gmail.com>
2025-05-02 21:24:32 +0530

    auto-commit

--- src/app/(protected)/presentation/[presentationId]/_components/editor/MasterRecursiveComponent.tsx
+++ src/app/(protected)/presentation/[presentationId]/_components/editor/MasterRecursiveComponent.tsx
@@ -1,6 +1,8 @@
+'use client'
+import { Heading1 } from '@/components/global/editor/components/Headings';
 import { ContentItem } from '@/lib/types'
 import {motion} from 'framer-motion'
-import React from 'react'
+import React, {useCallback} from 'react'
 
 type MasterRecursiveComponentProps = {
   content: ContentItem;
@@ -16,11 +18,30 @@ type MasterRecursiveComponentProps = {
 
 
 const ContentRenderer: React.FC<MasterRecursiveComponentProps> = React.memo(
-  ({ content, onContentChange, slideId, index }) => {
+  ({ content, onContentChange, slideId, index,isPreview,isEditable }) => {
+    const handleChange = useCallback(
+        (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+          onContentChange(content.id, e.target.value);
+        },
+        [content.id, onContentChange]
+      );
+      
+      const commonProps = {
+        value: content.content as string,
+        placeholder: content.placeholder,
+        onChange: handleChange,
+        isPreview: isPreview,
+      }
+      const animationProps = {
+        initial: { opacity: 0, y: 20 },
+        animate: { opacity: 1, y: 0 },
+        transition: { duration: 0.5 },
+      }
+
     switch (content.type) {
       case 'heading1':
         return <motion.div className="w-full h-full">
-            
+            <Heading1 {...commonProps}/>
         </motion.div>;
       default:
         return null;
